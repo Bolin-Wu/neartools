1605d9708e26930179bd619cb84f4c8cc009a3ec
Bolin Wu <bolin.wu1026@gmail.com>
2023-01-23 16:09:50 +0100

    update sample data and get_label_df function

---
+++ R/get_label_df.R
@@ -0,0 +1,20 @@
+#' Get labels from a dataset
+#'
+#' During work, many times we receive SPSS or STATA data files with labels. This function is to extract the labels from the read-in data, turn them in tibble format.
+#' This can help us to inspect data more conveniently.
+#'
+#' @param df_w_label A data set with labels. One can read SPSS or STATA files with \code{haven} package.
+#' @importFrom sjlabelled get_label
+#' @import tibble
+#' @return A tibble with variable and label names
+#'
+#' @export
+#' @examples
+#' get_label_df(fake_snack_df)
+
+get_label_df <- function(df_w_label) {
+  label_char <- sjlabelled::get_label(df_w_label)
+  label_df <- tibble::rownames_to_column(as.data.frame(label_char), "variable")
+  label_df <- tibble::as_tibble(label_df)
+  return(label_df)
+}
---
+++ data-raw/fake_caide_df.R
@@ -0,0 +1,14 @@
+## code to prepare `dta_exp` dataset goes
+types <- "ddddddddddddddddddDDddddddd"
+
+fake_caide_df = vroom::gen_tbl(rows = 1000, col_types = types, missing = 0.1)
+
+colnames(fake_caide_df) = c("time", "terv48", "fu1_age", "fu2_age", "terv45", "terv46",
+  "q67", "q68a", "q69", "sys1", "sys2", "f_systo1", "f_systo2",
+  "f_diasto1", "f_terve60", "f_terve61", "f_terve63", "f_terve64",
+  "fu1_date", "fu2_date", "fup", "women", "edu_yrs", "id", "base_date",
+  "age_base", "dem_incl_regs")
+
+
+
+usethis::use_data(fake_caide_df, overwrite = TRUE)
