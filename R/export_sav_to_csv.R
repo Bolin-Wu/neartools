d0d557ee82991bc7fb0d78b3d5e89aaecdcef500
romba050 <basilerommes@hotmail.com>
2023-02-06 14:15:13 +0100

    added out_dir argument to export_sav_to_csv

--- R/export_sav_to_csv.R
+++ R/export_sav_to_csv.R
@@ -15,6 +15,7 @@
 #'
 #' @param data_folder_name The name of the folder where all the data are being stored.
 #' @param db_name The name of specific database's folder, which is located in `data_folder_name`.
+#' @param out_dir optional. The name of the output directory, instead of using <data_folder_name>/<db_name>/csv_format.
 #'
 #' @return A folder called "csv_format" will be generated in the 'db_name's folder, storing all the transformed csv files.
 #'
@@ -35,15 +36,21 @@
 #'
 #'
 
-export_sav_to_csv <- function(data_folder_name, db_name) {
+export_sav_to_csv <- function(data_folder_name, db_name, out_dir=NULL) {
   db_dir <- here::here(data_folder_name, db_name)
   tb_name <- list.files(path = db_dir, pattern = "\\.sav$")
   # get rid of .sav extension when writing the csv file
   clean_name <- file_path_sans_ext(tb_name)
   # replace space with underscore
   clean_name <- gsub(" ", "_", clean_name)
+
+  # depending on optional user argument output_folder, the function can write to output_folder instead of csv_format
   # use file.path to create a file path that is compatible on different OS.
-  output_dir <- file.path(data_folder_name, db_name, "csv_format")
+  if(!is.null(out_dir)){
+    output_dir <- out_dir
+  }else{
+    output_dir <- file.path(data_folder_name, db_name, "csv_format")
+  }
 
   if (!dir.exists(output_dir)) {
     # create a folder to store the transformed csv format files
