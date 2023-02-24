46ba29b0fd14c27c9629ed7fac733cd7c18ac798
Bolin Wu <bolin.wu1026@gmail.com>
2023-02-24 12:12:16 +0100

    add pdf template to get_pretty_template() funciton

--- R/get_pretty_template.R
+++ R/get_pretty_template.R
@@ -3,19 +3,22 @@
 #' @param name Name of rmd file. Please do not use '/' in the the file name (no need to add suffix ".Rmd").
 #' @param subDir Name of the folder where rmd file will be created. Remember to connect sub-dir names with '/'. By default it is 'rmd'.
 #' @param open Should the file be opened after being created
-#' @param output_file "word" or "html", by default it is "html"
+#' @param output_file "word", "pdf" or "html", by default it is "html"
 #' @param ... Arguments to be passed to \link[usethis]{use_template}
 #' @importFrom usethis use_template
 #' @importFrom here here
 #' @examples
 #' \dontrun{
-#'  neartools::get_pretty_template(subDir = 'Enquiry/SNAC',name = "missing_value_problems",output_file = "word")
+#' neartools::get_pretty_template(
+#'   subDir = "Enquiry/SNAC", name = "missing_value_problems",
+#'   output_file = "word"
+#' )
 #' }
 #'
 #' @export
 get_pretty_template <-
   function(name = NULL,
-           subDir = 'rmd',
+           subDir = "rmd",
            output_file = "html",
            open = interactive(),
            ...) {
@@ -27,26 +30,32 @@ get_pretty_template <-
 
     # create a folder for Rmd
     if (dir.exists(subDir)) {
-      message("folder:", subDir," already exists.")
+      message("folder:", subDir, " already exists.")
     } else {
       message("Create folder: ", subDir, ".")
-      dir.create(subDir,  showWarnings = TRUE, recursive = TRUE)
+      dir.create(subDir, showWarnings = TRUE, recursive = TRUE)
     }
 
     # usethis::use_package("usethis")
     if (output_file == "html") {
       usethis::use_template("skeleton_html.Rmd",
-        save_as = paste0(subDir, "/",name),
+        save_as = paste0(subDir, "/", name),
         data = list(),
         package = "neartools", ..., open = open
       )
     } else if (output_file == "word") {
       usethis::use_template("skeleton_word.Rmd",
-        save_as = paste0(subDir, "/",name),
+        save_as = paste0(subDir, "/", name),
+        data = list(),
+        package = "neartools", ..., open = open
+      )
+    } else if (output_file == "pdf") {
+      usethis::use_template("skeleton_pdf.Rmd",
+        save_as = paste0(subDir, "/", name),
         data = list(),
         package = "neartools", ..., open = open
       )
     } else {
-      stop("The 'output_file' should be 'html' or 'word'")
+      stop("The 'output_file' should be 'html', 'pdf' or 'word'")
     }
   }
