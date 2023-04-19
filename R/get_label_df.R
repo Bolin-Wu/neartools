4e675f871e672e55a5d6cb3194ec36fa7ab33da3
Bolin Wu <bolin.wu1026@gmail.com>
2023-04-19 18:52:38 +0200

    add a na_ratio column to the result of get_label_df

--- R/get_label_df.R
+++ R/get_label_df.R
@@ -6,8 +6,14 @@
 #' @param df_w_label A data set with labels. One can read SPSS or STATA files with \code{haven} package.
 #' @importFrom sjlabelled get_label
 #' @importFrom glue glue
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_longer
+#' @importFrom dplyr rename
 #' @import tibble
-#' @return A tibble with variable and label names
+#' @return A tibble with three columns:
+#'   \item{variable}{The original variables' names.}
+#'   \item{n_files}{The variables corresponding labels.}
+#'   \item{na_ratio}{Ratio of missing values.}
 #'
 #' @export
 #' @examples
@@ -27,6 +33,15 @@ get_label_df <- function(df_w_label) {
   }
   label_char <- sjlabelled::get_label(df)
   label_df <- tibble::rownames_to_column(as.data.frame(label_char), "variable")
-  label_df <- tibble::as_tibble(label_df)
+  label_df <- tibble::as_tibble(label_df) %>% rename(variable = 1) # make sure the first column is "variable"
+  n_obs <- nrow(df_w_label)
+  # get the NA ratio
+  NA_df <- df_w_label %>%
+    summarise(across(everything(), ~ sum(is.na(.)))) %>%
+    pivot_longer(everything(),
+      names_to = "variable", values_to = "na_count"
+    ) %>%
+    transmute(variable, na_ratio = na_count / n_obs)
+  label_df <- label_df %>% left_join(NA_df)
   return(label_df)
 }
