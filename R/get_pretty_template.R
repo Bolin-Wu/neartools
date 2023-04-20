433fbdf3daaf5c4b76b43f0edab6d4891bff24d2
Bolin Wu <bolin.wu1026@gmail.com>
2023-04-20 15:55:08 +0200

    udpate documentation

--- R/get_pretty_template.R
+++ R/get_pretty_template.R
@@ -1,5 +1,5 @@
 #' @title get_pretty_template
-#' @description Automatically create & open an rmd file with a nice looking template. This facilitates the communication with researchers & tracking the records. User can choose output file to be word”, “pdf” or “html”. By default it complies an “html” file.
+#' @description Automatically create & open an rmd file with a nice looking template. This facilitates the communication with researchers & tracking the records. User can choose output file to be word”, “pdf” or “html”. By default it complies an “word” file.
 #' @param name Name of rmd file. Please do not use '/' in the the file name (no need to add suffix ".Rmd").
 #' @param subDir Name of the folder where rmd file will be created. Remember to connect sub-dir names with '/'. By default it is 'rmd'.
 #' @param open Should the file be opened after being created
