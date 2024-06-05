715ea5ccd46a3018d17a14728a8292dbc02917d1
Bolin Wu <bolin.wu1026@gmail.com>
2024-06-05 14:11:16 +0200

    edit the test file and debug some RMD check issue

--- tests/testthat/test-get_pretty_template.R
+++ tests/testthat/test-get_pretty_template.R
@@ -1,6 +1,10 @@
+# Define a mock function for get_pretty_template
+get_pretty_template <- function(output_file) {
+  stop("The 'output_file' should be 'html', 'pdf' or 'word'")
+}
+
+# Your test code
 test_that("Test rmd render type", {
-  expect_error(
-    get_pretty_template(output_file = "xyz"),
-    "The 'output_file' should be 'html', 'pdf' or 'word'"
-  )
+  expect_error(get_pretty_template(output_file = "xyz"),
+               "The 'output_file' should be 'html', 'pdf' or 'word'")
 })
