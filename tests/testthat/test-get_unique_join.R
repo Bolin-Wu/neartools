ed1c3bf3f16c696472cb05cb2c3831d68423f2e1
Bolin Wu <bolin.wu1026@gmail.com>
2024-06-10 10:58:40 +0200

    add some more tests

--- tests/testthat/test-get_unique_join.R
+++ tests/testthat/test-get_unique_join.R
@@ -4,3 +4,20 @@ test_that("Test the input for table names", {
                     join_type = "full_join",
                     by_cols = "Lopnr"), "Please input two tibbles' names in the argument `tibble_names`.")
 })
+
+
+test_that("Test the input for join_type", {
+  expect_error(
+    get_unique_join(tibble_names =  c("fake_snacn_ph_wave3", "fake_snacn_ph_fu"),
+                    join_type = "join",
+                    by_cols = "Lopnr"))
+})
+
+
+
+test_that("Test the input for by_cols", {
+  expect_error(
+    get_unique_join(tibble_names =  c("fake_snacn_ph_wave3", "fake_snacn_ph_fu"),
+                    join_type = "left_join",
+                    by_cols = "Lopnr = N1lopnr, age"), "Error: Single equal sign '=' detected.")
+})
